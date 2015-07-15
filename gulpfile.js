31374187de59e2ca052502d5110bcf6cc2e8dde5
Nastya <nstkmkm@gmail.com>
2015-07-15 03:22:17 +0300

    gulpfile.js done

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,29 @@
 var gulp = require('gulp'),
+    browserSync = require('browsersync'),
+    connect = require('gulp-connect'),
+    opn = require('opn'),
+    wiredep = require('wiredep').stream,
+    useref = require('gulp-useref'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    clean = require('gulp-clean'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concatCSS = require('gulp-concat-css'),
+    rename = require('gulp-rename'),
+    jade = require('jade'),
+    sass = require('gulp-sass');
 
 
 
-
+//
+gulp.task('server', function () {
+browserSync({
+port: 9000,
+server: {
+baseDir: 'app'
+}
+});
 
 gulp.task('default', function() {
-… задача поумолчанию
+
 });
