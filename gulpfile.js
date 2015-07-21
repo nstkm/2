c890561ae6872f64280aa214ac981c6559df3a83
Nastya <nstkmkm@gmail.com>
2015-07-21 15:20:41 +0300

    html done

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,50 @@
-var gulp = require('gulp'),
-    browserSync = require('browsersync'),
-    connect = require('gulp-connect'),
-    opn = require('opn'),
-    wiredep = require('wiredep').stream,
-    useref = require('gulp-useref'),
-    gulpif = require('gulp-if'),
-    uglify = require('gulp-uglify'),
-    clean = require('gulp-clean'),
-    autoprefixer = require('gulp-autoprefixer'),
-    concatCSS = require('gulp-concat-css'),
+var gulp = require("gulp"),
+    jade = require('gulp-jade'),
+    sass = require('gulp-sass'),
     rename = require('gulp-rename'),
-    jade = require('jade'),
-    sass = require('gulp-sass');
+    prefix = require('gulp-autoprefixer')
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
 
+gulp.task('server', ['jade'] , function () {
+  browserSync({
+    port: 9000,
+    server: {
+      baseDir: 'app'
+    }
+  });
+});
 
+gulp.task('watch', function () {
+    gulp.watch('app/scss/**/*.scss', ['css']);
+    gulp.watch('app/jade/**/*.jade', ['jade']);
+    gulp.watch([
+    //'./*.html',
+    'app/js/**/*.js',
+    'app/css/**/*.css'
+  ]).on('change', browserSync.reload);
+});
 
-//
-gulp.task('server', function () {
-browserSync({
-port: 9000,
-server: {
-baseDir: 'app'
-}
-});
+gulp.task('default', ['server', 'watch']);
 
-gulp.task('default', function() {
+gulp.task('jade', function() {
+gulp.src('app/jade/index.jade')
+.pipe(jade({ pretty: true }))
+.pipe(gulp.dest('app/'))
+.pipe(reload({stream: true}));
+});
 
-});
+gulp.task('css', function () {
+        gulp.src('app/scss/main.scss')
+        //.pipe(concatCss('app/css/style.css'))
+        .pipe(prefix('last 2 versions','>1%','ie 7'))
+        .pipe(sass())
+        //.pipe(minifyCss())
+        .pipe(rename('main.css'))
+        .pipe(gulp.dest('app/css'))
+        .pipe(reload({stream: true}));
+        //.pipe(connect.reload());
+});
+
+//default
+//gulp.task('default', ['css', 'jade', 'watch']);
